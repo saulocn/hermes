ad272394aba4f030344d4e4ecff8117a58734e33
Saulo de Carvalho <saulocn@gmail.com>
2021-05-25 17:27:48 -0300

    Tratando e salvando o objeto de forma correta

--- hermes-api/controller/message/message.js
+++ hermes-api/controller/message/message.js
@@ -1,18 +1,41 @@
 const router = require('express').Router()
 const service = require('../../service/MessageService')
+const {pluck} = require('../../validation/pluck')
 
 module.exports = (app) => {
   router
     .route('/')
     .post(async (req, res) => {
-      let c
       try {
-        console.log(req.body)
-        c = await service.sendMessage(req.body)
+        const message = pluck(req.body, 'title', 'text', 'recipients')
+        await service.sendMessage(message)
+        res.status(202).json(message)
       } catch (err) {
         console.error('erro', err)
+        res.status(500).json(err)
       }
-      res.send(c)
+    }
+    )
+    .get(async (req, res) => {
+      try {
+        const messages = await service.findMessages()
+        res.status(202).json(messages)
+      } catch (err) {
+        console.error('erro', err)
+        res.status(500).json(err)
+      }
+    }
+    )
+
+    router
+    .get('/:id', async (req, res) => {
+      try {
+        const messages = await service.findMessage(req.params.id)
+        res.status(202).json(messages)
+      } catch (err) {
+        console.error('erro', err)
+        res.status(500).json(err)
+      }
     }
     )
   app.use('/messages', router)
--- hermes-api/service/MessageService.js
+++ hermes-api/service/MessageService.js
@@ -1,10 +1,23 @@
 const db = require('../data/db')
+const ObjectId = require('mongodb').ObjectId;
 const producer = require('../pulsar/MessageProducer')
 const COLLECTION_NAME = 'messages'
 
 const sendMessage = async (message) => {
+    message.sent = false
+    message.created = new Date()
+    console.log(message)
     await db.getConnection().collection(COLLECTION_NAME).insertOne(message)
     producer.sendMessage(message)
+    return message
 }
 
-module.exports = { sendMessage }
+const findMessages = async () => {
+    return await db.getConnection().collection(COLLECTION_NAME).find().toArray()
+}
+
+const findMessage = async (id) => {
+    return await db.getConnection().collection(COLLECTION_NAME).find({_id : new ObjectId(id)}).toArray()
+}
+
+module.exports = { sendMessage, findMessages, findMessage }
